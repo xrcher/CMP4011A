0b104639bbb83e3bd55756e1207254fd83222833
VJTetsuro <137433782+VJTetsuro@users.noreply.github.com>
2024-12-08 04:54:08 +0000

    Changes

--- scripts/unsdg.js
+++ scripts/unsdg.js
@@ -1,5 +1,63 @@
-//=========================================unsdg.js========================================//
+//=========================================unsdg.js========================================\\
 
+const titleElement1 = document.querySelector("#goal1title");
+const titleElement2 = document.querySelector("#goal2title");
+const titleElement3 = document.querySelector("#goal3title");
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("json/unsdg.json")
+        .then((response) => response.json())
+        .then((responseData) => {
+            if (Array.isArray(responseData)) {
+                responseData.forEach((item) => {
+
+                    //Goal 1
+                    const goal1title = titleElement1;
+                    goal1title.textContent = item.a1;
+
+                    //Goal 2
+                    const goal2title = titleElement2;
+                    goal2title.textContent = item.b1;
+
+                    //Goal 3
+                    const goal3title = titleElement3;
+                    goal3title.textContent = item.c1;
+
+                });
+            } else {
+                console.error("Invalid JSON format: Expected an array");
+            }
+        })
+        .catch((error) => console.error("Error fetching JSON data:", error));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const sectionElement01 = document.querySelector("#goal1Contentbox");
 
 
@@ -32,3 +90,4 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .catch((error) => console.error("Error fetching JSON data:", error));
 });
+*/
