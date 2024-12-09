121b6eb4bd0fa85cdfc1056cef256a69992b3cd5
Archer <archergamer30@gmail.com>
2024-12-09 12:20:44 +0000

    NPM Server Finished

--- node-express/app/app.js
+++ node-express/app/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
 res.send("Hello, welcome to my NodeJS app!");
 });
+
 app.listen(port, () => {
 console.log(`myapp is listening on port ${port}!`);
 })
