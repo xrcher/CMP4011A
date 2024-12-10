289fba81caabf6263fb351fbec25988b993bbe9c
PJ162 <pjfausto2346@gmail.com>
2024-12-10 06:05:26 +0000

    Final Changes

--- node-express/app/public/scripts/unsdg.js
+++ node-express/app/public/scripts/unsdg.js
@@ -4,6 +4,7 @@ const titleElement1 = document.querySelector("#goal1title");
 const titleElement2 = document.querySelector("#goal2title");
 const titleElement3 = document.querySelector("#goal3title");
 const goal1contentElement = document.querySelector("#goal1Contentbox");
+const goal2contentElement = document.querySelector("#goal2Contentbox");
 const goal3contentElement = document.querySelector("#goal3Contentbox");
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -17,37 +18,44 @@ document.addEventListener("DOMContentLoaded", () => {
                     const goal1title = titleElement1;
                     goal1title.textContent = item.a1;
                     const goal1content= document.createElement("article");
-                    const goal1para1=document.createElement("p");
                     const goal1stat1=document.createElement("p");
-                    const goal1para2=document.createElement("p");
-                    const goal1stat2=document.createElement("p");
                     goal1stat1.setAttribute("class","statistic");
-                    goal1stat2.setAttribute("class","statistic");
-                    goal1stat1.textContent=item.a3;
-                    goal1para1.setAttribute("class","goalCardtext");
-                    goal1para2.setAttribute("class","goalCardtext");
-                    goal1para1.textContent= item.a2;
-                    goal1para2.textContent=item.a4;
-                    goal1stat2.textContent=item.a5
+                    goal1stat1.textContent= item.a2;
+    
                     
 
-                    goal1content.appendChild(goal1para1)
-                    goal1content.appendChild(goal1stat1)
-                    goal1content.appendChild(goal1para2)
-                    goal1content.appendChild(goal1stat2)
-                    
-                    goal1contentElement.appendChild(goal1content)
+                    goal1content.appendChild(goal1stat1);
+                    goal1contentElement.appendChild(goal1content);
 
 
 
                     //Goal 2
                     const goal2title = titleElement2;
                     goal2title.textContent = item.b1;
+                    const goal2content=document.createElement("article");
+                    const goal2stat1=document.createElement("p");
+                    goal2stat1.textContent=item.b2;
+                    goal2stat1.setAttribute("class","statistic")
+
+                    goal2content.appendChild(goal2stat1);
+                    goal2contentElement.appendChild(goal2content);
+
 
                     //Goal 3
                     const goal3title = titleElement3;
-                    goal3title.textContent=item.c1
+                    goal3title.textContent=item.c1;
                     const goal3content=document.createElement("article");
+                    const goal3stat1=document.createElement("p");
+                    goal3stat1.textContent=item.c2;
+                    goal3stat1.setAttribute("class","statistic")
+
+                    goal3content.appendChild(goal3stat1)
+                    goal3contentElement.appendChild(goal3stat1);
+
+
+
+                    
+                   
 
                     
 
@@ -85,37 +93,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-/*
-const sectionElement01 = document.querySelector("#goal1Contentbox");
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("json/unsdg.json")
-        .then((response) => response.json())
-        .then((responseData) => {
-            if (Array.isArray(responseData)) {
-                responseData.forEach((item) => {
-
-                    //goal1Text
-                    // const goal1Contentbox=document.createElement("article");
-                    // goal1Contentbox.setAttribute("class","goal1txt");
-                    
-                    const goal1Text=document.createElement("p");
-                    goal1Text.textContent=" "+item.p1;
-                    // goal1Contentbox.appendChild(goal1Text)
-                    sectionElement01.appendChild(goal1Text);
-
-
-
-
-
-
-
-                });
-            } else {
-                console.error("Invalid JSON format: Expected an array");
-            }
-        })
-        .catch((error) => console.error("Error fetching JSON data:", error));
-});
-*/
