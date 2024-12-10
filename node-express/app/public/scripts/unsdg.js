2b8720a0b933b2dd2814cb9de972f5834ad5ae29
PJ162 <pjfausto2346@gmail.com>
2024-12-10 04:55:27 +0000

    Changes to unsdg.html
    Functional buttons, json updated

--- node-express/app/public/scripts/unsdg.js
+++ node-express/app/public/scripts/unsdg.js
@@ -3,6 +3,8 @@
 const titleElement1 = document.querySelector("#goal1title");
 const titleElement2 = document.querySelector("#goal2title");
 const titleElement3 = document.querySelector("#goal3title");
+const goal1contentElement = document.querySelector("#goal1Contentbox");
+const goal3contentElement = document.querySelector("#goal3Contentbox");
 
 document.addEventListener("DOMContentLoaded", () => {
     fetch("json/unsdg.json")
@@ -14,6 +16,29 @@ document.addEventListener("DOMContentLoaded", () => {
                     //Goal 1
                     const goal1title = titleElement1;
                     goal1title.textContent = item.a1;
+                    const goal1content= document.createElement("article");
+                    const goal1para1=document.createElement("p");
+                    const goal1stat1=document.createElement("p");
+                    const goal1para2=document.createElement("p");
+                    const goal1stat2=document.createElement("p");
+                    goal1stat1.setAttribute("class","statistic");
+                    goal1stat2.setAttribute("class","statistic");
+                    goal1stat1.textContent=item.a3;
+                    goal1para1.setAttribute("class","goalCardtext");
+                    goal1para2.setAttribute("class","goalCardtext");
+                    goal1para1.textContent= item.a2;
+                    goal1para2.textContent=item.a4;
+                    goal1stat2.textContent=item.a5
+                    
+
+                    goal1content.appendChild(goal1para1)
+                    goal1content.appendChild(goal1stat1)
+                    goal1content.appendChild(goal1para2)
+                    goal1content.appendChild(goal1stat2)
+                    
+                    goal1contentElement.appendChild(goal1content)
+
+
 
                     //Goal 2
                     const goal2title = titleElement2;
@@ -21,7 +46,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
                     //Goal 3
                     const goal3title = titleElement3;
-                    goal3title.textContent = item.c1;
+                    goal3title.textContent=item.c1
+                    const goal3content=document.createElement("article");
+
+                    
 
                 });
             } else {
